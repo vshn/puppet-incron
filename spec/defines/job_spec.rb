060de1016134f3c67ec1939e96c4cc5eb739616a
Eugene Piven <thepegasd@gmail.com>
2017-02-08 23:38:41 +0300

    incron::job tests

--- spec/defines/job_spec.rb
+++ spec/defines/job_spec.rb
@@ -7,9 +7,18 @@ describe 'incron::job' do
     let(:params) { {
       :command => '/usr/bin/process_file',
       :path    => '/upload',
-      :event   => 'IN_CLOSE_WRITE',
+      :event   => 'IN_MOVED_TO',
     } }
 
     it { is_expected.to compile.with_all_deps }
+    it { is_expected.to contain_class('incron') }
+
+    it { is_expected.to contain_file('/etc/incron.d/process_file').with(
+      :owner   => 'root',
+      :group   => 'root',
+      :mode    => '0644',
+      :content => /^\/upload IN_MOVED_TO \/usr\/bin\/process_file$/,
+    ) }
+
   end
 end
