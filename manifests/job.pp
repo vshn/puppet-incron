e219c5f888040d77fea6351e3fbd3a45f584747c
Eugene Piven <thepegasd@gmail.com>
2017-02-08 23:27:31 +0300

    incron::job basic implementation

---
+++ manifests/job.pp
@@ -0,0 +1,26 @@
+# incron::job class
+#
+# Primary incron resource used to create incron jobs.
+#
+# @author Eugene Piven <epiven@gmail.com>
+#
+# @see https://github.com/pegasd/puppet-incron
+#
+# @example Using incron resource
+#   incron::job { 'process_file':
+#     path    => '/upload',
+#     event   => 'IN_CLOSE_WRITE',
+#     command => '/usr/bin/process_file',
+#   }
+#
+# @param command Command to execute on triggered event
+# @param event inotify event (either 'IN_CLOSE_WRITE' or 'IN_MOVED_TO')
+# @param path Path to watched directory
+# @param mode Incron job file permissions, which is located at /etc/incron.d/JOB_NAME
+define incron::job (
+  String                                $command,
+  Enum['IN_CLOSE_WRITE', 'IN_MOVED_TO'] $event,
+  Stdlib::Unixpath                      $path,
+  Pattern[/^[0-7]{4}$/]                 $mode = '0644',
+) {
+}
