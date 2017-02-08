4e628c7531a8652b25fb9fc1527173c2b53fe4ad
Eugene Piven <thepegasd@gmail.com>
2017-02-08 21:40:44 +0300

    Initial incron class implementation

---
+++ manifests/init.pp
@@ -0,0 +1,14 @@
+# incron class
+#
+# Main entry point for incron class which must be included
+# in order to start managing all incron-related resources.
+#
+# @author Eugene Piven <epiven@gmail.com>
+#
+# @see https://github.com/pegasd/puppet-cron
+#
+# @example Declaring incron
+#   require incron
+class incron {
+
+}
