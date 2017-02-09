a56211f85ab77fdf9b6328863f6636d8b4b90c0a
Eugene Piven <thepegasd@gmail.com>
2017-02-09 09:43:08 +0300

    Minor documentation change for incron::whitelist

--- manifests/whitelist.pp
+++ manifests/whitelist.pp
@@ -1,6 +1,8 @@
 # incron::whitelist resource
 #
 # Use it to whitelist any system incron jobs you don't want to touch.
+# This will make sure that /etc/incron.d/JOB_NAME won't get deleted nor
+# modified.
 #
 # @author Eugene Piven <epiven@gmail.com>
 #
