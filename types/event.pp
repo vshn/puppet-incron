5e1f68583cd60c6934aa3a6e85c233b40121e0e0
Eugene Piven <thepegasd@gmail.com>
2018-03-03 20:09:31 +0300

    Allow lots more inotify events

--- types/event.pp
+++ types/event.pp
@@ -1,1 +1,22 @@
-type Incron::Event = Enum['IN_CLOSE_WRITE', 'IN_MOVED_TO']
+# inotify event type
+type Incron::Event = Enum[
+  'IN_ALL_EVENTS',
+  'IN_ACCESS',
+  'IN_ATTRIB',
+  'IN_CLOSE_WRITE',
+  'IN_CLOSE_NOWRITE',
+  'IN_CREATE',
+  'IN_DELETE',
+  'IN_DELETE_SELF',
+  'IN_MODIFY',
+  'IN_MOVE_SELF',
+  'IN_MOVED_FROM',
+  'IN_MOVED_TO',
+  'IN_OPEN',
+
+  'IN_DONT_FOLLOW',
+  'IN_ONESHOT',
+  'IN_ONLYDIR',
+
+  'IN_NO_LOOP',
+]
