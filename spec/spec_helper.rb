92dbb3c2ead96aa8696156f5fd7b4f808aeab52d
Eugene Piven <thepegasd@gmail.com>
2018-03-03 11:03:07 +0300

    Fix broken specs due to package purging

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
-  c.formatter = :documentation
+  c.formatter     = :documentation
+  c.default_facts = {
+    osfamily: 'Debian',
+  }
   c.after(:suite) { RSpec::Puppet::Coverage.report! }
 end
