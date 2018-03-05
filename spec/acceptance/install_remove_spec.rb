1d3eacc8023ddef6f4da7f561be30f0c226f9058
Eugene Piven <thepegasd@gmail.com>
2018-03-05 22:55:28 +0300

    Fix acceptance tests

--- spec/acceptance/install_remove_spec.rb
+++ spec/acceptance/install_remove_spec.rb
@@ -3,13 +3,6 @@
 require 'spec_helper_acceptance'
 
 describe 'incron' do
-  managed_files = %w[
-    /etc/incron.allow
-    /etc/incron.deny
-    /etc/incron.conf
-    /etc/incron.d
-  ]
-
   context 'installs?' do
     pp = <<~PUPPET
 
@@ -27,15 +20,22 @@ describe 'incron' do
       it { is_expected.to be_running }
     end
 
-    managed_files.each do |present_file|
-      describe file(present_file) do
-        it { is_expected.to exist }
-      end
+    describe file('/etc/incron.allow') do
+      it { is_expected.to exist }
+      its(:content) { is_expected.to match('') }
+    end
+
+    describe file('/etc/incron.conf') do
+      it { is_expected.to exist }
+      its(:content) { is_expected.to match('') }
     end
 
     describe file('/etc/incron.d') do
+      it { is_expected.to exist }
       it { is_expected.to be_directory }
     end
+
+    describe file('/etc/incron.deny') { it { is_expected.not_to exist } }
   end
 
   context 'removes?' do
@@ -54,6 +54,13 @@ describe 'incron' do
     describe service('incron') do
       it { is_expected.not_to be_running }
     end
+
+    managed_files = %w[
+      /etc/incron.allow
+      /etc/incron.deny
+      /etc/incron.conf
+      /etc/incron.d
+    ]
 
     managed_files.each do |absent_file|
       describe file(absent_file) do
