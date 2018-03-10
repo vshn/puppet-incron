e16cdf58e2f2fcfc11363750a107f2cf547c5061
Eugene Piven <thepegasd@gmail.com>
2018-03-10 15:18:45 +0300

    Acceptance tests for incrontab(1) usage, incron::whitelist

---
+++ spec/acceptance/regular_user_incrontab_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require 'spec_helper_acceptance'
+
+describe 'incrontab(1)' do
+  context 'luke is not ready yet' do
+    pp = <<~PUPPET
+
+      include incron
+
+      user { 'luke': ensure => present }
+
+    PUPPET
+
+    apply_and_test_idempotence pp
+
+    describe command('sudo -u luke env EDITOR=cat incrontab -e') do
+      its(:exit_status) { is_expected.to eq 1 }
+      its(:stderr) { is_expected.to match(/^user 'luke' is not allowed to use incron$/) }
+    end
+  end
+
+  context 'luke has force' do
+    pp = <<~PUPPET
+
+      class { 'incron':
+        allowed_users => [ 'luke' ],
+      }
+
+      user { 'luke': ensure => present }
+
+    PUPPET
+
+    apply_and_test_idempotence pp
+
+    describe command('sudo -u luke env EDITOR=cat incrontab -e') do
+      its(:exit_status) { is_expected.to eq 0 }
+      its(:stderr) { is_expected.to match('') }
+    end
+  end
+
+  context 'clean up' do
+    pp = <<~PUPPET
+
+      include incron
+
+      user { 'luke': ensure => absent }
+
+    PUPPET
+
+    apply_and_test_idempotence pp
+
+    describe user('luke') { it { is_expected.not_to exist } }
+  end
+end
